f8fc70bd999ba559421120d2c4238ecc4d9e4328
fejnartal <fejnartal@gmail.com>
2017-05-23 01:35:08 +0200

    Random hardcoded vocabulary as in the original code.

---
+++ src/main/java/hangman/HardcodedVocabulary.java
@@ -0,0 +1,16 @@
+package hangman;
+
+import java.util.Random;
+
+public final class HardcodedVocabulary implements SecretProvider {
+    private static final String[] WORDS = {
+            "simplicity", "equality", "grandmother",
+            "neighborhood", "relationship", "mathematics",
+            "university", "explanation"
+        };
+
+	@Override
+	public final SecretPhrase provideSecret() {
+		return new SecretPhrase(WORDS[new Random().nextInt(WORDS.length)]);
+	}
+}
