fe65ea197d760ace4d205114877aec33cf0fc561
fejnartal <fejnartal@gmail.com>
2017-05-23 02:52:34 +0200

    Simple CLI interface.
    Possible future improvements.
    Ascii animation?
    Improved SecretProviders?
    Multilanguage support?

--- src/main/java/hangman/Main.java
+++ src/main/java/hangman/Main.java
@@ -14,85 +14,29 @@
  */
 package hangman;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.util.Iterator;
-import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
-
-    private final InputStream input;
-    private final OutputStream output;
-    private final int max;
-    private static final String[] WORDS = {
-        "simplicity", "equality", "grandmother",
-        "neighborhood", "relationship", "mathematics",
-        "university", "explanation"
-    };
-
-    public Main(final InputStream in, final OutputStream out, final int m) {
-        this.input = in;
-        this.output = out;
-        this.max = m;
-    }
-
-    public static void main(final String... args) {
-        new Main(System.in, System.out, 5).exec();
-    }
-
-    public void exec() {
-        String word = WORDS[new Random().nextInt(WORDS.length)];
-        boolean[] visible = new boolean[word.length()];
-        int mistakes = 0;
-        try (final PrintStream out = new PrintStream(this.output)) {
-            final Iterator<String> scanner = new Scanner(this.input);
-            boolean done = true;
-            while (mistakes < this.max) {
-                done = true;
-                for (int i = 0; i < word.length(); ++i) {
-                    if (!visible[i]) {
-                        done = false;
-                    }
-                }
-                if (done) {
-                    break;
-                }
-                out.print("Guess a letter: ");
-                char chr = scanner.next().charAt(0);
-                boolean hit = false;
-                for (int i = 0; i < word.length(); ++i) {
-                    if (word.charAt(i) == chr && !visible[i]) {
-                        visible[i] = true;
-                        hit = true;
-                    }
-                }
-                if (hit) {
-                    out.print("Hit!\n");
-                } else {
-                    out.printf(
-                        "Missed, mistake #%d out of %d\n",
-                        mistakes + 1, this.max
-                    );
-                    ++mistakes;
-                }
-                out.append("The word: ");
-                for (int i = 0; i < word.length(); ++i) {
-                    if (visible[i]) {
-                        out.print(word.charAt(i));
-                    } else {
-                        out.print("?");
-                    }
-                }
-                out.append("\n\n");
-            }
-            if (done) {
-                out.print("You won!\n");
+public final class Main {
+	public final static void main( String[] args ) {
+		final Scanner scan = new Scanner(System.in);
+		final Hangman hangman = new Hangman(new HardcodedVocabulary());
+		while(hangman.gameStage() == Hangman.Stage.PLAYING) {
+            System.out.print("Guess a letter: ");
+            final char c = scan.next().charAt(0);
+            final GuessRound round = hangman.discover(c);
+            if( round.missed ) {
+            	System.out.printf("Missed, mistake #%d out of %d\n", round.mistakes, hangman.allowedMistakes);
             } else {
-                out.print("You lost.\n");
+            	System.out.println("Hit!");
             }
-        }
+            // Replace non-printable NULL characters with CLI friendly alternative
+            final String printableHints = round.partialSolution.replace('\0','_');
+            System.out.println("The word: " + printableHints);
+		}
+		if(hangman.gameStage() == Hangman.Stage.YOUWON) {
+            System.out.println("You won!");
+		} else {
+			System.out.println("You lost.\n");
+		}
     }
-
 }
