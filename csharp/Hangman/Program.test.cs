4f94e00a22280b4f8732b1622b1ec544efa6e743
Giorgi Koguashvili <kogo27@gmail.com>
2017-06-08 18:26:38 +0400

    Added implementation on C#

---
+++ csharp/Hangman/Program.test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Hangman
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Method()
+        {
+            var output = new MemoryStream();
+            new Program(
+                new MemoryStream(
+                    Encoding.ASCII.GetBytes("a\na\na\na\na\n")
+                ), 
+                output, 
+                1
+            ).Exec();
+            Assert.Contains(
+                        "You lost", 
+                        Encoding.ASCII.GetString(output.ToArray())
+                    );
+        }
+    }
+}
