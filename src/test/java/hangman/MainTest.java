15e69a11a790b9bd03e3f67f68518477f45feb1c
Vlad Stelmakh <vlad1k9369@gmail.com>
2023-07-30 00:26:57 +0600

    Fix test

--- src/test/java/hangman/MainTest.java
+++ src/test/java/hangman/MainTest.java
@@ -1,38 +1,29 @@
 package hangman;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.Assert.assertFalse;
 
 public final class MainTest {
 
     @Test
     public void failsAfterManyWrongAttempts() {
         final ByteArrayInputStream input = new ByteArrayInputStream(
-                "1\n1\n1\n1\n1\n".getBytes()
+                "1\n2\n".getBytes()
         );
 
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-
         Secret secret = new Secret();
-        new Hangman(
-                new Mistakes(
-                        new Attempt(
-                                new Guess(input),
-                                secret
-                        ),
-                        secret,
-                        1
-                )
+        Boolean gameResult = new Hangman(
+                new Attempt(
+                        new UserInput(input),
+                        secret
+                ),
+                secret,
+                1
         ).play();
 
-        assertThat(outContent.toString(), containsString("You lost"));
+        assertFalse(gameResult);
     }
-
 }
